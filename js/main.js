8d60a48b0a77f6a1db3ea40a7caa1ab5a8afa1bd
zsoszi <zsolt.szilagyi86@gmail.com>
2021-01-10 22:30:47 +0100

    final task
    ES6 modification

--- js/main.js
+++ js/main.js
@@ -1,38 +1,64 @@
-const hECMAthlon = {
+class hECMAthlon {
 
-    getMaxValue: function (input) {
-        var maxValues = input.split(",");
-        var maxValue = 0;
-        for(var i = 0; i < maxValues.length; i++){
-            var tempValue = parseInt(maxValues[i]);
+    getMaxValue (input) {
+        let maxValues = input.split(",");
+        let bigger = (a,b)=>{
+            if(parseInt(a) > parseInt(b)){
+                return a;
+            }else{
+                return b;
+            }
+        }
+       
+        /* 
+        let maxValue = 0;
+        for(let i = 0; i < maxValues.length; i++){
+            let tempValue = parseInt(maxValues[i]);
             if (tempValue > maxValue) {
                 maxValue = tempValue;
             }
-        }
-        return [maxValue];
-    },
+        } 
+        */
 
-    getGreaterThan: function (input) {
-        var greaterValues = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-        var greaterNums = [];
-        for(var j = 0; j < greaterValues.length; j++) {
+        let maxValue = maxValues.reduce(bigger);
+        return [maxValue];
+    } 
+
+    getGreaterThan (input) {
+        let greaterValues = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].filter( n => n > parseInt(input));
+        
+        /* 
+        let greaterNums = [];
+        for(let j = 0; j < greaterValues.length; j++) {
             if (greaterValues[j] > parseInt(input)) {
                 greaterNums.push(greaterValues[j]);
             }
         }
-        return greaterNums;
-    },
+        */
+        return greaterValues; 
+    }
 
-    fizzBuzz: function (input) {
-        var output = [];
-        for (var k = 1; k <= parseInt(input); k++) {
-            var value;
-            if(k % 3 === 0) value = "Fizz";
+    fizzBuzz (input) {
+        let newArray = [...Array(parseInt(input))];
+        let output = newArray.map((currentNumber, index) => {
+            let k = index+1;
+            let value = k;
+            if(k % 5 === 0 && k % 3 === 0) value = "FizzBuzz";
+            else if(k % 3 === 0) value = "Fizz";
             else if(k % 5 === 0) value = "Buzz";
-            else if(k % 5 === 0 && k % 3 === 0) value = "FizzBuzz";
             else value = k;
-            output.push(value);
-        }
+            return value;
+
+        });
+        /* 
+        
+        let output = [];
+        for (let k = 1; k <= parseInt(input); k++) {
+            let value;
+        */
+            
+        //    output.push(value);
+        
         return output;
     }
 }
